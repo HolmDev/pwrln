8a51c8b39fc160cc68ee19ab182910639e5573c8
HolmDev <noak.fredholm@gmail.com>
2021-03-17 12:53:11 +0100

    Changed the way git branches are discovered

--- _set.bat
+++ _set.bat
@@ -1,10 +1,6 @@
-        @echo off
-        for %%a in ("%CD%") do set "PARENT_FOLDER=%%~nxa"
-        title %PARENT_FOLDER%
-        set GITBRANCH=
-        for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
-        if "%GITBRANCH%" == "" (
-            prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
-        ) else (
-            prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
-        )
+@echo off
+::Gets the current git branch and passes it along to _prompt.bat
+set GITBRANCH=
+for /f "tokens=*" %%g in ('git.exe branch 2^> NUL') do (set GITBRANCH=%%g)
+if "%GITBRANCH%" == "" %pwrln_dir%_prompt.bat normal
+if %GITBRANCH:~0,1% == * %pwrln_dir%_prompt.bat git %GITBRANCH:~2%
